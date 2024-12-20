1ceedc7820b71a647f5fea62273416b6bebfcbf9
Anoopkr1906 <anoopkumar19062006@gmail.com>
2024-12-20 16:29:10 +0000

    JS datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" ; // treat all JS code as a newer version .
+
+// alert(3+3) // not allowed as we are using nodejs not browser 
+
+console.log("Anoop Kumar Burnwal")
+
+let name = "Anoop"
+let age = 18
+let isLoggedIn = false 
+
+// number -> 2^53;
+// bigint
+// string => ""
+// boolean => true/false 
+// null => standalone value or empty value but reserved 
+// undefined => 
+// symbol => for uniqueness 
+
+console.log(typeof "Anoop")
+console.log(typeof null) // object type 
+console.log(typeof undefined) // type undefined 
