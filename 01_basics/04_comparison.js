b04e9d9bf6b13bc1c574e0bb1cd8a33a3ace42f8
Anoopkr1906 <anoopkumar19062006@gmail.com>
2024-12-21 13:06:31 +0000

    comparison.JS

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+console.log(2>1)
+console.log(2>=1)
+
+
+console.log(null > 0)     // false
+console.log(null == 0)    // false
+console.log(null >= 0)    // true
+
+console.log(undefined > 0)     // false
+console.log(undefined == 0)    // false
+console.log(undefined >= 0)    // false
+
+
+// === strictly checks the datatype also
+
+console.log("2" === 2)   // false as datatype is diff.
